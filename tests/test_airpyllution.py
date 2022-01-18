2a7c9bc128e498bc880362afe2a9831c0fbcd747
mel-liow <mel.liow.94@gmail.com>
2022-01-17 21:59:12 -0800

    feat: refactor tests

--- tests/test_airpyllution.py
+++ tests/test_airpyllution.py
@@ -1,7 +1,35 @@
 from airpyllution import airpyllution
 import pandas as pd
+from pandas._testing import assert_frame_equal
+from unittest.mock import patch
+from constants import *
+from airpyllution.utils import *
 
-def test_pollution_history():
+
+def mocked_requests_get_pollution_history(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+    print(kwargs)
+    if kwargs['url'] == 'http://api.openweathermap.org/data/2.5/air_pollution/history':
+
+        if kwargs['params']['appid'] == 'invalid_api_key':
+            return MockResponse(mock_api_invalid_key_error, 404)
+
+        return MockResponse(mock_history_data, 200)
+
+    elif kwargs['url'] == 'http://api.openweathermap.org/data/2.5/air_pollution/forecast':
+        return MockResponse(mock_forecast_data, 200)
+    
+    return MockResponse({'cod': 401, 'message': 'Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.' }, 404)
+
+
+@patch('requests.get', side_effect=mocked_requests_get_pollution_history)
+def test_pollution_history(mock_api_call):
     """Test word counting from a file."""
     mock_params = {
         'lat': 49.28,
@@ -16,9 +44,10 @@ def test_pollution_history():
         'lon': 'longitude_val',
         'start': 1234.567,
         'end': 3.14159,
-        'appid': 'fake'
+        'appid': 'invalid_api_key'
     }
 
+    # Invalid input type
     assert airpyllution.get_pollution_history(
         mock_incorrect_params['start'], 
         mock_params['end'], 
@@ -26,6 +55,7 @@ def test_pollution_history():
         mock_params['lon'], 
         mock_params['appid']) == "start_date input should be an int"
 
+    # Invalid input type
     assert airpyllution.get_pollution_history(
         mock_params['start'], 
         mock_incorrect_params['end'], 
@@ -33,6 +63,7 @@ def test_pollution_history():
         mock_params['lon'], 
         mock_params['appid']) == "end_date input should be an int"
 
+    # Invalid input type
     assert airpyllution.get_pollution_history(
         mock_params['start'], 
         mock_params['end'], 
@@ -40,6 +71,7 @@ def test_pollution_history():
         mock_params['lon'], 
         mock_params['appid']) == "Latitude input should be a float"
     
+    # Invalid input type
     assert airpyllution.get_pollution_history(
         mock_params['start'], 
         mock_params['end'], 
@@ -47,9 +79,21 @@ def test_pollution_history():
         mock_incorrect_params['lon'], 
         mock_params['appid']) == "Longitude input should be a float"
 
+    # Invalid API key
     assert airpyllution.get_pollution_history(
         mock_params['start'], 
         mock_params['end'], 
         mock_params['lat'], 
         mock_params['lon'], 
         mock_incorrect_params['appid']) == 'Invalid API key. Please see http://openweathermap.org/faq#error401 for more info.'
+
+    # Test for correct pandas output
+    pollution_data_frame = airpyllution.get_pollution_history(
+        mock_params['start'], 
+        mock_params['end'], 
+        mock_params['lat'], 
+        mock_params['lon'], 
+        mock_params['appid'])
+    
+    assert_frame_equal(pollution_data_frame, convert_data_to_pandas(mock_history_data))
+
