22a022f66e2f7ac4c3e166d3bbfb67543a851cc8
christopheralex <christopher.a0@gmail.com>
2022-01-19 10:28:04 -0800

    test: Adds another test case for pollution forecast

--- tests/test_airpyllution.py
+++ tests/test_airpyllution.py
@@ -4,6 +4,7 @@ from pandas._testing import assert_frame_equal
 from unittest.mock import patch
 from constants import *
 from airpyllution.utils import *
+import altair as alt
 
 
 def mocked_requests_get_pollution(*args, **kwargs):
@@ -131,3 +132,5 @@ def test_pollution_forecast(mock_api_call):
     assert len(forecast_chart.data) > 2
     assert len(forecast_chart.data) == 16
     assert forecast_chart.title == "Pollutant concentration for the next 5 days"
+    chart_dict = forecast_chart.to_dict()
+    assert chart_dict["facet"] == alt.Facet("Pollutants:N").to_dict()
