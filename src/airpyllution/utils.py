e7269113282259b0f69b513b26b11a2d6ea78c3b
Daniel King <dnlkng13@gmail.com>
2022-01-20 22:54:54 -0800

    fix: Modify data conversion function to work with all functions that call it

--- src/airpyllution/utils.py
+++ src/airpyllution/utils.py
@@ -40,11 +40,12 @@ def convert_data_to_pandas(raw_data):
     --------
     >>> convert_data_to_pandas(data)
     """ 
-    try:
+    if len(raw_data["list"]) > 1:
         data = pd.DataFrame.from_records(list(map(lambda x:x["components"],raw_data["list"])))
         data["dt"] = list(map(lambda x:convert_unix_to_date(x["dt"]),raw_data["list"]))
-    except:
-        data = raw_data["coord"]
+    else:
+        data  = raw_data["coord"]
         data.update(raw_data["list"][0]["components"])
         data = pd.DataFrame.from_records([data])
+
     return data
